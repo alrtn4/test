f8da755d3b98398fdaa46d5bcba828601f2e90c3
alrtn4 <45690266+alrtn4@users.noreply.github.com>
2019-01-22 10:31:37 +0330

    big change in layout

---
+++ app/src/main/java/com/example/ideapad510/sherkat/controler/DataControler.java
@@ -0,0 +1,13 @@
+package com.example.ideapad510.sherkat.controler;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Ideapad 510 on 1/22/2019.
+ */
+/*
+public class DataControler extends AppCompatActivity implements DataFlowControler {
+
+}
+*/
